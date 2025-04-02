ff8a0a4d9a66e7631c3fcb767d2ce42ae9e9a5e2
xallmean <izalll097@gmail.com>
2025-04-03 06:07:46 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -18,7 +18,7 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/b17248930f58bbdaf2e23.jpg",
+                "https://telegra.ph/file/94c726f014cbc932c6c19-8c08a98cd3922dc12e.jpg",
                 caption="𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
                 buttons=[(Button.url("Store", "https://t.me/jasebxall")),
                          (Button.url("Support", "https://t.me/ixallsupport"))]
