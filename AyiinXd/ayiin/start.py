f1b3e39d726002a8a519d2ce49a356a9a2770ab8
xallmean <izalll097@gmail.com>
2025-04-14 21:18:54 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -15,14 +15,16 @@ async def startupmessage():
     Start up message in telegram logger group
     """
     try:
-        if BOTLOG:
-            await tgbot.send_file(
-                BOTLOG_CHATID,
-                "https://telegra.ph/file/c3fe5f881e4f65cd40f13.jpg",
-                caption="𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
-                buttons=[(Button.url("Store", "https://t.me/jasebxall")),
-                         (Button.url("Support", "https://t.me/ixallsupport"))]
+        if BOTLOG:  # Pastikan BOTLOG udah didefinisikan sebelumnya
+            await tgbot.send_message(
+                BOTLOG_CHATID,  # Pastikan BOTLOG_CHATID udah didefinisikan sebelumnya
+                "𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                file="https://telegra.ph/file/c3fe5f881e4f65cd40f13.jpg",  # Menggunakan URL gambar
+                buttons=[
+                    [Button.url("Store", "https://t.me/jasebxall")],
+                    [Button.url("Support", "https://t.me/ixallsupport")]
+                ]
             )
     except Exception as e:
-        LOGS.error(e)
+        LOGS.error(f"Error: {e}")
         return None
